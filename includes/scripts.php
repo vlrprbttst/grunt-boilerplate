848ea802eeee5483b4869d742f996dcdfb140895
Valerio Pierbattista <valeriopierbattista@yahoo.it>
2015-03-20 12:55:01 +0100

    structure changes
    structure changes

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,3 +1,2 @@
 		<!-- JS scripts -->
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 		<script src="js/build/production.min.js"></script>
--- index.php
+++ index.php
@@ -1,18 +1,4 @@
-<!doctype html>
-<!--[if IE 9]>
-<html lang="en" class="ie ie9">
-<![endif]-->
-<!--[if lt IE 9]>
-<html lang="en" class="ie ie-legacy">
-<![endif]-->
-<html lang="en">
-	<head>
-		<style type="text/css">
-			<?php include 'css/critical.css.php'; ?>
-		</style>
-		<?php include 'includes/head.php'; ?>
-	</head>
-	
+<?php include 'includes/head.php'; ?>
 	<body>
 		<?php include 'includes/navigation.php'; ?>
 		<?php include 'includes/footer.php'; ?>
